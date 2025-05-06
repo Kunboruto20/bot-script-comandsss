f61e8d8aa1644150a86b7003efb01e31d53e3924
gyovannyvpn123 <mdanut159@gmail.com>
2025-05-06 03:58:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pkg install nodejs -y
 pkg install git -y
 pkg install jq -y
 npm install -g npm
-npm install @whiskeysockets/baileys
+npm install @borutowaileys/library
 npm install
 clear
 npm start 
