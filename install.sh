6b9b4b54c59782f7c6a9f10d1d13cb90616f9427
gyovannyvpn123 <mdanut159@gmail.com>
2025-06-15 17:06:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pkg install nodejs -y
 pkg install git -y
 pkg install jq -y
 npm install -g npm
-npm install @borutowaileys/library@latest
+npm install @borutowaileys/library qrcode-terminal pino chalk
 npm install
 clear
 npm start 
